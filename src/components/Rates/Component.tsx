5ebe158d51e78ad8de5d82b32fc7535e907a8e42
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-09-11 14:38:34 +0300

    Get currency data on mount

--- src/components/Rates/Component.tsx
+++ src/components/Rates/Component.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useMemo } from 'react'
 import ApexChart, { Props } from 'react-apexcharts'
 import { TReduxProps } from './Container'
 import { StyledContainer } from './style'
@@ -6,10 +6,50 @@ import { StyledContainer } from './style'
 export type TComponentProps = {
 } & TReduxProps
 
-const Rates: React.FC<TComponentProps> = () => {
+const Rates: React.FC<TComponentProps> = ({
+  getChartData,
+  startDate,
+  endDate,
+  currencyID,
+  data
+}) => {
+  useEffect(
+    () => {
+      getChartData(startDate, endDate, currencyID)
+    },
+    [
+      getChartData,
+      startDate,
+      endDate,
+      currencyID
+    ]
+  )
 
-  const chartOptions = {}
-  const chartSeries = []
+  const chartOptions = useMemo(
+    () => (
+      {
+        chart: {
+          zoom: {
+            enabled: false
+          }
+        },
+        xaxis: {
+          labels: {
+            datetimeUTC: false
+          },
+          type: 'datetime'
+        }
+      }
+    ),
+    []
+  )
+  const chartSeries = useMemo(
+    () => [{
+      name: "Rates",
+      data
+    }],
+    [data]
+  )
   const type = 'line'
 
   return (
