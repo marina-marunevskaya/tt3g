2a962aa69fbea22e6c96dbe0bc8f4d29aa4fb463
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-09-11 13:45:45 +0300

    mapStateToProps and mapActionsToProps were configured

--- src/components/Rates/Container.ts
+++ src/components/Rates/Container.ts
@@ -2,13 +2,27 @@ import { connect, ConnectedProps } from 'react-redux'
 
 import { IAppState } from 'store'
 import Component from './Component'
+import {
+  getChartData,
+  setChartCurrency
+} from 'actions/chart'
 
 const mapStateToProps = (state: IAppState) => ({
-  fetching: state.common.fetching
+  fetching: state.common.fetching,
+  startDate: state.chart.startDate,
+  endDate: state.chart.endDate,
+  currencyID: state.chart.currencyID,
+  error: state.chart.error,
+  data: state.chart.data
 })
 const mapActionsToProps = (dispatch) => ({
+  getChartData: (startDate, endDate, currencyID) => dispatch(
+    getChartData(startDate, endDate, currencyID)
+  ),
+  setChartCurrency: (currencyID) => dispatch(
+    setChartCurrency(currencyID)
+  )
 })
-
 
 const connector = connect(mapStateToProps, mapActionsToProps)
 export type TReduxProps = ConnectedProps<typeof connector>
