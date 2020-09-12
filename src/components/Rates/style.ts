01e014c053565ec51f9a4a73eee28ea454946522
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-09-12 16:54:18 +0300

    Styles were updated.

--- src/components/Rates/style.ts
+++ src/components/Rates/style.ts
@@ -1,6 +1,18 @@
 import styled from 'styled-components'
 
 export const StyledContainer = styled.div`
+  align-items:center;
+  background:#fff;
   display:flex;
+  flex-direction:column;
+  justify-content:center;
+  padding:50px 0;
+`
+
+export const StyledSpinnerContainer = styled.div`
+  height:40px;
+  align-items:center;
+  display:flex;
+  flex-direction:column;
   justify-content:center;
 `
